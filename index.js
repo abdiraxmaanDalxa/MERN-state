be24ae273c759f27a1180648939f1dbf90425e79
abdiraxmaan dalxa <abdirahmansaeed@gmail.com>
2024-03-25 15:11:09 +0300

    created user model schema

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from "dotenv"
-dotenv.config();
-mongoose.connect(process.env.db)
 
 const app = express();
 app.listen(3000,() =>{
