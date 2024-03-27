b1b3e7c1ca99bc209478e78ab0eb0444cf83da87
abdiraxmaan dalxa <abdirahmansaeed@gmail.com>
2024-03-27 12:23:53 +0300

    Compelete singup

--- controller/singUpController.js
+++ controller/singUpController.js
@@ -6,8 +6,20 @@ export const singup = async (req,res) =>{
         data:{name ,email,password}
     })
 
+    console.log(newUser,"user is created successfully")
     return res.json(newUser).status(200);
     } catch (error) {
         return res.json(error.message).status(400)
     }
 };
+
+export const getall = async(req ,res) =>{
+    try {
+        const gell = await prisma.user.findMany();
+        res.json(gell).status(200)
+    } catch (error) {
+        return res.json(error.message).status(400)
+    }
+}
+
+
--- router/singUpRouter.js
+++ router/singUpRouter.js
@@ -1,7 +1,8 @@
 import  Express, { Router } from "express";
-import { singup } from "../controller/singUpController.js";
+import { getall, singup } from "../controller/singUpController.js";
 const routes = Router();
 
 routes.post("/",singup)
+routes.get("/",getall)
 
 export default routes;
